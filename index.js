50c01839613dcf16c4413a04ba64ac3cdb07c0e0
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-02-25 13:57:28 -0700

    trying to stabilize connection

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+const router = require('express').Router();
+const apiRoutes = require('./routes/api');
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error!</h1>');
 });
+
+module.exports = router;
 
 
 
