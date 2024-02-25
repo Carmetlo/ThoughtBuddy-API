0f194c67deacc6364f3fc0cb299984ae9226de1b
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-02-25 13:37:29 -0700

    initial setup of thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,91 @@
+const {Thought, User, Reaction} = require('../models');
+const {Types, get} = require('mongoose');
+
+const thoughtController = {
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({});
+            res.json(thoughts);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+},
+
+async getThoughtById(req, res) {
+    try {
+        const thought = await Thought.findOne({_id:req.params.thoughtId});
+        if (!thought) {
+            return res.status(404).json({message: 'No thought found!'});
+        } else {
+            res.json(thought);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+async createThought(req, res) {
+    try {
+        const thought = await Thought.create(req.body);
+        res.status(201).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+async deleteThought(req, res) {
+    try {
+        const thought = await Thought.findOneAndDelete({_id:req.params.thoughtId});
+        res.status(404).json({message: 'No thought found!'});
+        } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+async updateThought(req, res) {
+    try {
+        const thought = await Thought.findOneAndUpdate(req.params.thoughtId, req.body,{
+            new: true,
+        });
+        if (!thought) {
+            res.status(404).json({message: 'No thought found!'});
+        } else {
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$addToSet: {reactions: req.body}},
+                {runValidators: true, new: true}
+            );
+            thought ? res.json(thought) : res.status(404).json({message: 'No thought found!'});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+    async removeReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$pull: {reactions: {reactionId: req.params.reactionId}}},
+                {runValidators: true, new: true}
+            );
+
+            thought ? res.json(thought) : res.status(404).json({message: 'No thought found!'});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+};
+
+module.exports = thoughtController;
+
