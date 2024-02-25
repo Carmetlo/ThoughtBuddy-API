5c13c33ab51fe38d56797e297b19db8b51d8c53b
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-02-25 15:00:20 -0700

    added faker users, thoughts and reactions

--- models/faker.js
+++ models/faker.js
@@ -1,22 +1,35 @@
 const faker = require('faker');
 const mongoose = require('mongoose');
 const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
 
-mongoose.connect('mongodb://localhost:27017/yourDatabaseName', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/ThoughtBuddy', { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function seedUsers() {
- await User.deleteMany({});
-
+async function seedDatabase() {
  for (let i = 0; i < 100; i++) {
     const username = faker.internet.userName();
     const email = faker.internet.email();
     const user = new User({ username, email });
-    
     await user.save();
+
+    for (let j = 0; j < 10; j++) {
+        const thoughtText = faker.lorem.sentence();
+        const thought = new Thought({ thoughtText, username });
+        await thought.save();
+
+        for (let k = 0; k < 3; k++) {
+            const reactionBody = faker.lorem.words();
+            const reaction = new Reaction({ reactionBody, username });
+            thought.reactions.push(reaction);
+        }
+            await thought.save();
+        }
+
      }
 
-    console.log('Users seeded');
+    console.log('Data seeded');
     process.exit();
 }
 
-seedUsers();
+seedDatabase();
