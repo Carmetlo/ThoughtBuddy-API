def2e3cdec9e01a1c11ae8fb49ec994264658331
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-02-25 13:39:15 -0700

    fixed syntax errors in thoughtcontroller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,49 +10,53 @@ const thoughtController = {
             console.log(err);
             res.status(500).json(err);
         }
-    }
-},
+    },
 
-async getThoughtById(req, res) {
-    try {
-        const thought = await Thought.findOne({_id:req.params.thoughtId});
-        if (!thought) {
-            return res.status(404).json({message: 'No thought found!'});
-        } else {
-            res.json(thought);
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-},
-
-async createThought(req, res) {
-    try {
-        const thought = await Thought.create(req.body);
-        res.status(201).json(thought);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-},
-
-async deleteThought(req, res) {
-    try {
-        const thought = await Thought.findOneAndDelete({_id:req.params.thoughtId});
-        res.status(404).json({message: 'No thought found!'});
+    async getThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOne({_id:req.params.thoughtId});
+            if (!thought) {
+                return res.status(404).json({message: 'No thought found!'});
+            } else {
+                res.json(thought);
+            }
         } catch (err) {
-        res.status(500).json(err);
-    }
-},
+            res.status(500).json(err);
+        }
+    },
 
-async updateThought(req, res) {
-    try {
-        const thought = await Thought.findOneAndUpdate(req.params.thoughtId, req.body,{
-            new: true,
-        });
-        if (!thought) {
-            res.status(404).json({message: 'No thought found!'});
-        } else {
-            res.json(thought);
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.status(201).json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({_id:req.params.thoughtId});
+            if (!thought) {
+                res.status(404).json({message: 'No thought found!'});
+            } else {
+                res.json(thought);
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate({_id: req.params.thoughtId}, req.body, {
+                new: true,
+            });
+            if (!thought) {
+                res.status(404).json({message: 'No thought found!'});
+            } else {
+                res.json(thought);
+            }
         } catch (err) {
             res.status(500).json(err);
         }
@@ -84,8 +88,6 @@ async updateThought(req, res) {
             res.status(500).json(e);
         }
     },
-
 };
 
 module.exports = thoughtController;
-
